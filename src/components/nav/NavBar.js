f9b670ccf28d6d9d78f4f88775f54a6d54163b47
David Billups <davidtbillups@gmail.com>
2021-03-10 11:02:35 -0500

    created auth

--- src/components/nav/NavBar.js
+++ src/components/nav/NavBar.js
@@ -9,7 +9,7 @@ class NavBar extends Component {
             <nav className="navbar bg-dark text-white flex-md-nowrap p-0 shadow">
                 <ul className="nav nav-pills nav-fill">
                     <li className="nav-item">
-                        <Link className="nav-link" to="/">Articles</Link>
+                        <Link className="nav-link" to="/articles">Articles</Link>
                     </li>
                     <li className="nav-item">
                         <Link className="nav-link" to="/friends">Friends</Link>
