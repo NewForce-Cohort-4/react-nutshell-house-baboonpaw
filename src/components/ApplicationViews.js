9e7910c0f081705763959c897b78406072abfb91
David Billups <davidtbillups@gmail.com>
2021-03-11 11:21:33 -0500

    finished with MVP Ticket 5

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -1,55 +1,24 @@
+import React from "react";
 import { Route } from "react-router-dom";
-import React, { Component } from "react";
+import { MessageProvider } from "./Message/MessageProvider"
+import { MessageList } from "./Message/MessageList"
 
-export default class ApplicationViews extends Component {
 
-  render() {
-    return (
-      <React.Fragment>
+export const ApplicationViews = () => {
+  return (
+      <>
+          {/* Render the location list when http://localhost:3000/ */}
+{/*           
+          <Route exact path="/">
+              <Home />
+          </Route> */}
 
-        <Route
-          exact path="/" render={props => {
-            return null
-            // Remove null and return the component which will show news articles
-          }}
-        />
-
-        <Route
-          exact path="/register" render={props => {
-            return null
-            // Remove null and return the component which will handle user registration
-          }}
-        />
-
-        <Route
-          path="/friends" render={props => {
-            return null
-            // Remove null and return the component which will show list of friends
-          }}
-        />
-
-        <Route
-          path="/messages" render={props => {
-            return null
-            // Remove null and return the component which will show the messages
-          }}
-        />
-
-        <Route
-          path="/tasks" render={props => {
-            return null
-            // Remove null and return the component which will show the user's tasks
-          }}
-        />
-
-        <Route
-          path="/events" render={props => {
-            return null
-            // Remove null and return the component which will show the user's events
-          }}
-        />
-
-      </React.Fragment>
-    );
-  }
+          {/* Render the animal list when http://localhost:3000/messages */}
+          <MessageProvider>
+              <Route exact path="/messages">
+                  <MessageList />
+              </Route>
+          </MessageProvider>
+      </>
+  )
 }
