50959e6c1e0dc31916e46020d63f36401f337296
Detective-Coder <aaronfrankenfield@gmail.com>
2021-03-12 14:27:15 -0500

    everything works, ticket 1 complete

--- src/components/articles/ArticleSaveForm.js
+++ src/components/articles/ArticleSaveForm.js
@@ -30,10 +30,19 @@ export const ArticleForm = () => {
   }
 
   const handleSaveArticle = (event) => {
-    event.preventDefault()
+    if (article.title === "") {
+      window.alert("Please add a title")
+    } else if (article.synopsis === '') {
+      window.alert("Please add a synopsis")
+    } else if (article.url === '') {
+      window.alert("Please add a url")
+    } else {
+      event.preventDefault()
 
-    addArticle(article)
-    .then(() => history.push("/articles"))
+      addArticle(article)
+      .then(() => history.push("/articles"))
+    }
+    
   }
 
   return (
@@ -49,13 +58,13 @@ export const ArticleForm = () => {
         <fieldset>
           <div className="form-group">
             <label htmlFor="synopsis">Synopsis: </label>
-            <input type="textarea" id="synopsis" name="synopsis" className="form-control" onChange={handleControlledInputChange}/>
+            <input type="textarea" id="synopsis" name="synopsis" required className="form-control" onChange={handleControlledInputChange}/>
           </div>
         </fieldset>
         <fieldset>
           <div className="form-group">  
             <label htmlFor="url">URL: </label>
-            <input type="text" id="url" name="url" className="form-control" onChange={handleControlledInputChange}/>
+            <input type="url" id="url" name="url" className="form-control" required onChange={handleControlledInputChange}/>
           </div>
         </fieldset>
         <button className="btn btn-primary" onClick={handleSaveArticle}>Save Article</button>
